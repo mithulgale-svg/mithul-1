2d853f3e71287d72f0b221e8b76bc7eea957d03d
Mithul E S <mithulgale@gmail.com>
2026-06-16 10:21:26 +0530

    Add intern.txt and mitu.py

---
+++ mitu.py
@@ -0,0 +1,98 @@
+
+# task1
+# python_course={"arun","megha","rahul","sneha"}
+# data_analytics={"rahul","sneha","aman","diya"}
+
+# print("students in both courses:",python_course & data_analytics)
+# print("students only in python:",python_course-data_analytics)
+# print("students only in data_analytics:",data_analytics-python_course)
+# print("all students:",python_course|data_analytics)
+# print("exactly one course:",python_course^data_analytics)
+
+
+# 
+# task2
+# customer1={"samsung","apple","oneplus","realme"}
+# customer2={"samsung","apple","vivo","oppo"}
+
+# print("common brands:",customer1&customer2)
+# print("all brands:",customer1|customer2)
+# print("only by customer1:",customer1-customer2)
+# print("only one customer:",customer1^customer2)
+
+# task3
+
+# design_team={"figma","photoshop","canva","illustrator"}
+# marketing_team={"canva","excel","photoshop","powerbi"}
+# print("both teams:",design_team & marketing_team)
+# print("only design team:",design_team-marketing_team)
+# print("all tools used:",design_team|marketing_team)
+# print("only one team:",design_team^marketing_team)
+
+
+# task4
+
+# cricket_fans={"ajay","kiran","rohit","nisha"}
+# football_fans={"nisha","rahul","ajay","david"}
+
+# print("both sports:",cricket_fans&football_fans)
+# print("only cricket:",cricket_fans-football_fans)
+# print("only football:",football_fans-cricket_fans)
+# print("all sports:",cricket_fans|football_fans)
+
+
+# task4
+
+# electronics={"laptop","headphones","mouse","keyboard"}
+# accessories={"keyboard","mouse pad","laptop stand","headphones"}
+
+# print("common:",electronics&accessories)
+# print("only in electronics:",electronics-accessories)
+# print("all items:",electronics|accessories)
+# print("only one:",electronics^accessories)
+
+# task5
+
+# emp1={"python","java","sql","c"}
+# emp2={"python","javascript","sql","go"}
+
+# print("common:",emp1&emp2)
+# print("only by emp1:",emp1-emp2)
+# print("all lang:",emp1|emp2)
+# print("only one:",emp1^emp2)
+
+# task6
+
+# frnd1={"netflix","prime","hotstar","soyliv"}
+# frnd2={"netflix","zee5","prime","jio cinema"}
+
+# print("common:",frnd1&frnd2)
+# print("unique to frnd1:",frnd1-frnd2)
+# print("all ott:",frnd1|frnd2)
+# print("only one:",frnd1^frnd2)
+
+# task8
+
+# avail_prod={"laptop","mouse","keyboard","monitor"}
+# sold={"mouse","monitor"}
+
+# print("remaining:",avail_prod-sold)
+# print("sold:",sold)
+
+# avail_prod.add("printer")
+# print("after adding:",avail_prod)
+
+# avail_prod.remove("keyboard")
+# print("after removing:",avail_prod)
+
+# task9
+
+# task10
+
+
+campA={"user101","user102","user103","user104"}
+campB={"user103","user104","user105","user106"}
+
+print("common:",campA&campB)
+print("unique to campa:",campA-campB)
+print("")
